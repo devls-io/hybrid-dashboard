335fc2ed211c260fc3f300aa5901a7c01622f276
Leonardo Sudré <165829938+devls-io@users.noreply.github.com>
2025-11-30 17:32:53 -0300

    Feat: Implement hybrid blog with SSG posts, dark mode, and unit tests

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BotaoTema from "./components/BotaoTema";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +19,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors duration-300`}
       >
+
+        {/* Conteúdo da página */}
         {children}
+
+        {/* Botão Flutuante (CSR) */}
+        <BotaoTema/>
       </body>
     </html>
   );
